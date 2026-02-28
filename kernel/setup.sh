4a3c73c86b878e542a84f739a3d8e0106a7ae9d3
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-02-28 17:18:43 +0800

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -40,7 +40,7 @@ perform_cleanup() {
 
 # Sets up or update KernelSU-Next environment
 setup_kernelsu() {
-    echo "[+] Setting up $REPO..."
+    echo "[+] Setting up $OWNER/$REPO..."
     test -d "$GKI_ROOT/$REPO" || git clone "https://github.com/$OWNER/$REPO" && echo "[+] Repository cloned."
     cd "$GKI_ROOT/$REPO"
     git stash && echo "[-] Stashed current changes."
